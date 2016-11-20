822943000d0aeb8c93e2e1f1b4d518139a5b6801
Tobias Naftali <tobias.naftali@gmail.com>
2016-11-20 12:14:35 -0300

    Text changed

--- TextProvider.py
+++ TextProvider.py
@@ -1,21 +1,27 @@
 class TextProvider:
-    start = 'You can start using this bot searching by tag typing "@wallpaperss_bot" followed by a word, and it ' \
-           'will search images tagged with that word.\n\nYou can also use the /tags command to get five random tags,' \
+    start = 'You can start using this bot by typing "@wallpaperss_bot", that will show you the preview of ' \
+            'some wallpapers. You can also type a word after that, and it will search and show you the ' \
+            'preview of wallpapers tagged with that word.\nYou can then tap a wallpaper preview to get the full' \
+            'wallpaper.\n\n' \
+            'You can also use the /tags command to get some random tags that you could use to search,' \
             ' /random to get a random wallpaper, or /submit to submit an image to be reviewed and added to the' \
             ' wallpapers collection.'
 
-    help = 'You can search by tag typing "@wallpaperss_bot" followed by a word, and it ' \
-           'will search images tagged with that word.\n\n' \
-           'You can also use the /tags command to get five random tags, /random to get a random wallpaper, or /submit' \
-           ' to submit an image to be reviewed and added to the wallpapers collection.'
+    help = 'You can search wallpapers by typing "@wallpaperss_bot", that will show you the preview of ' \
+           'some wallpapers. You can also type a word after that, and it will search and show you the ' \
+           'preview of wallpapers tagged with that word.\nYou can then tap a wallpaper preview to get the full' \
+           'wallpaper.\n\n' \
+           'You can also use the /tags command to get some random tags that you could use to search,' \
+           ' /random to get a random wallpaper, or /submit to submit an image to be reviewed and added to the' \
+           ' wallpapers collection.'
 
     unknown = 'Sorry, I didn\'t understand that command. To get help on how to use the bot type the /help command.'
 
     upload = 'To upload an image to be reviewed and added to the wallpapers collection please send me the image ' \
-             'as a file.\nThe maximum permitted size is 5 MB.'
+             'as a file.\nThe format must be JPG, JPEG, or PNG.\nThe maximum permitted size is 5 MB.'
 
     upload_ok = 'Your image was uploaded successfully!\n\n It will be reviewed to be added to the wallpapers collection'
 
-    upload_type_error = 'The file must be an image (PNG, JPG, JPEG)'
+    upload_type_error = 'The file must be an image (JPG, JPEG, or PNG).'
 
     upload_unknown_error = 'There was an error uploading the image, please try again.'
